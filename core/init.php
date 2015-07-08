a1db812980f01a6257735ea13a9bf33ab2bef548
Miguel Vega <mvegaplm@gmail.com>
2015-07-08 16:31:53 -0500

    reloaded init
    core reloaded init and config database

--- core/init.php
+++ core/init.php
@@ -1,10 +1,6 @@
 <?php
 
-// $connect_error = 'Al parecer hubo un error en la conexion con la base de datos';
 
-// conection database
-// mysql_connect('localhost', 'miguel', 'miguel2658915') or die($connect_error);
-// mysql_select_db('aepme') or die ($connect_error);
 
 
 ?>
